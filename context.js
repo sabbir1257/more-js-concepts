2dbae48b2da5ff6341045c2106afe0d914fd2e09
sabbir1257 <masabbir1257@gmail.com>
2024-11-27 00:55:13 +0600

    Promise, Async/Await

--- context.js
+++ context.js
@@ -1,5 +1,9 @@
-let a =5;
-function add (num1, num2){
+let a = 5;
+
+function add(num1, num2) {
      const result = num1 + num2 + a;
      return result;
 }
+
+const sum = add(4, 6);
+console.log(sum);
